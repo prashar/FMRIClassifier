dac1b274941f8f91f6f557b35c6a2b259651a3ec
Neeraj Prashar <neeraj.prashar@gmail.com>
2014-02-22 23:07:18 -0800

    cleaning up logistic reg code

--- logistic_reg.py
+++ logistic_reg.py
@@ -3,6 +3,24 @@ from scipy.sparse import *
 from scipy.io import * 
 from sklearn import linear_model
 
+def TestPath(x_train,y_train,x_test,y_test,l1orl2='l1',C=1.0):
+  log_reg = linear_model.LogisticRegression(penalty=l1orl2,C=C)
+  log_reg.fit(fmri_train,y_train)
+  ans = log_reg.predict(fmri_test)
+  
+  mistakes = shape(where(ans != y_test.flatten())[0])[1]
+  test_error = mistakes/60. 
+  print "Test Mistakes", mistakes
+  print "Test Error ", test_error
+  
+  ans = log_reg.predict(fmri_train)
+  mistakes = shape(where(ans != y_train.flatten())[0])[1]
+  train_error = mistakes/300. 
+  print "Tr Mistakes", mistakes
+  print "Tr Error ", train_error
+
+  return (1.-train_error),(1.-test_error)
+
 fmri_train = mmread("fmri/subject1_fmri_std.train.mtx")
 fmri_test = mmread("fmri/subject1_fmri_std.test.mtx")
 
@@ -12,18 +30,17 @@ y_train = y_train.reshape(300,1)
 
 y_test = mmread("ytest_cat.out.mtx")
 y_test = y_test.todense()
-y_train = y_train.reshape(300,1)
+y_test = y_test.reshape(60,1)
 
-log_reg = linear_model.LogisticRegression()
-log_reg.fit(fmri_train,y_train)
-ans = log_reg.predict(fmri_test)
+results = zeros((10,3),dtype=float)
+idx=0
+for i in arange(1.0,0.0,-.1):
+  train_error,test_error = TestPath(fmri_train,y_train,fmri_test,y_test,l1orl2='l2',C=i)
+  print train_error, test_error
+  results[idx][0] = i 
+  results[idx][1] = train_error 
+  results[idx][2] = test_error 
+  #train_error,test_error = TestPath(fmri_train,y_train,fmri_test,y_test,l1orl2='l2')
+  idx += 1
 
-print ans
-print y_test
-
-print where(ans != y_test)
-mistakes = shape(where(ans != y_test)[0])[1]
-print "Mistakes", mistakes
-print "Error ", mistakes/60. 
-
-
+print results
