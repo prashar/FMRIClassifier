dd628998c2dad8bfad4c954f06f3961853e9e9cd
Neeraj Prashar <neeraj.prashar@gmail.com>
2014-02-23 18:59:48 -0800

    Adding the new logistic reg .. it has the split for 240/60

--- logistic_reg.py
+++ logistic_reg.py
@@ -8,39 +8,36 @@ def TestPath(x_train,y_train,x_test,y_test,l1orl2='l1',C=1.0):
   log_reg.fit(fmri_train,y_train)
   ans = log_reg.predict(fmri_test)
   
-  mistakes = shape(where(ans != y_test.flatten())[0])[1]
-  test_error = mistakes/60. 
-  print "Test Mistakes", mistakes
-  print "Test Error ", test_error
+  mistakes_test = shape(where(ans != y_test.flatten())[0])[0]
+  print "Test Mistakes", mistakes_test
   
   ans = log_reg.predict(fmri_train)
-  mistakes = shape(where(ans != y_train.flatten())[0])[1]
-  train_error = mistakes/300. 
-  print "Tr Mistakes", mistakes
-  print "Tr Error ", train_error
+  mistakes_train = shape(where(ans != y_train.flatten())[0])[0]
+  print "Tr Mistakes", mistakes_train
 
-  return (1.-train_error),(1.-test_error)
+  return (mistakes_train),(mistakes_test)
 
-fmri_train = mmread("fmri/subject1_fmri_std.train.mtx")
-fmri_test = mmread("fmri/subject1_fmri_std.test.mtx")
+fmri_train = mmread("fmri_train_240.out.mtx")
+train_rows = 240
+fmri_test = mmread("fmri_train_60.out.mtx")
+test_rows = 60
 
-y_train = mmread("ytrain_cat.out.mtx")
-y_train = y_train.todense()
-y_train = y_train.reshape(300,1)
+y_train = mmread("ytrain_240_cat.out.mtx")
 
-y_test = mmread("ytest_cat.out.mtx")
-y_test = y_test.todense()
-y_test = y_test.reshape(60,1)
+y_test = mmread("ytrain_60_cat.out.mtx")
 
-results = zeros((10,3),dtype=float)
+idx1 = arange(15,1,-1)
+results = zeros((35,3),dtype=int)
 idx=0
-for i in arange(1.0,0.0,-.1):
-  train_error,test_error = TestPath(fmri_train,y_train,fmri_test,y_test,l1orl2='l2',C=i)
+idx2 = array([1.0,.9,.8,.7,.6,.5,.4,.3,.2,.1,.08,.07,.05,.04,.03,.02,.01,.008,.004,.001],
+    dtype=float)
+arr = concatenate((idx1,idx2),0)
+for i in arr:
+  train_error,test_error = TestPath(fmri_train,y_train,fmri_test,y_test,l1orl2='l1',C=1.0/i)
   print train_error, test_error
   results[idx][0] = i 
   results[idx][1] = train_error 
   results[idx][2] = test_error 
-  #train_error,test_error = TestPath(fmri_train,y_train,fmri_test,y_test,l1orl2='l2')
   idx += 1
 
 print results
