7ba780eac30cb84bf495693572d983f50e30de74
Neeraj Prashar <neeraj.prashar@gmail.com>
2014-03-19 12:52:14 -0700

    Final Demo File

--- plotbrain.py
+++ plotbrain.py
@@ -22,8 +22,21 @@ def plotbrain(full_vox_coords, voxels_to_display):
     plt.show()
 
 # Does the actual plotting based on coordinates and what to display
-def plotsignificantbrain(full_vox_coords, rank_voxels):
+# We will be demoing this ... 
+def plotsignificantbrain(full_vox_coords, rank_voxels,fname):
+    print "Input shape of rank_voxels"
+    print np.shape(rank_voxels)
+    print "--",rank_voxels[0]
+    #rank_voxels =  np.array(rank_voxels,dtype='float')
+    print "Total non-zeros", np.shape(np.nonzero(rank_voxels)[0])
     fig = plt.figure()
+    print np.nonzero(rank_voxels)[0]
+    idx_all_positives = (rank_voxels >= 0)
+    print "positive indices"
+    print idx_all_positives.flatten
+    print "Total Trues: ", np.shape(np.where(idx_all_positives == True)[0])
+    print "Total false: ", np.shape(np.where(idx_all_positives == False)[0])
+    print type(idx_all_positives)
     ax = fig.add_subplot(111, projection='3d')
     xs = full_vox_coords[:,0]
     ys = full_vox_coords[:,1]
@@ -31,7 +44,17 @@ def plotsignificantbrain(full_vox_coords, rank_voxels):
     ax.set_xlabel('x voxels')
     ax.set_ylabel('y voxels')
     ax.set_zlabel('z voxels')
-    ax.scatter(xs, ys, zs, c=rank_voxels, cmap=plt.cm.coolwarm, marker='o')
+    ax.set_title('Voxel Activiation for Category - {0}'.format(fname))
+    xs = xs[idx_all_positives.flatten()]
+    ys = ys[idx_all_positives.flatten()]
+    zs = zs[idx_all_positives.flatten()]
+    #rank_voxels = np.arange(21764)
+    # Take only the ones we'll be actually plotting
+    color_rank = rank_voxels[idx_all_positives]
+    # Rank the data .. 
+    rank_voxels = rankdata(color_rank)
+    #ax.scatter(xs, ys, zs, c=rank_voxels, cmap=plt.cm.coolwarm, marker='o')
+    ax.scatter(xs, ys, zs, c=rank_voxels, cmap=plt.cm.YlGn, marker='o')
     plt.show()
 
 def plotfullbrain(coords):
@@ -87,9 +110,13 @@ coords = mmread("coord.out.mtx")
 #plot_imp_voxels_brain(coords)
 #exit()
 #fname = 'correct_weights/new_weight_vec15.out.mtx' 
-fname = 'fmri/subject1_fmri_std.train.mtx'
-data = mmread(fname)
+#fname = 'fmri/subject1_fmri_std.train.mtx'
+fname ='BrainCellWeightVectors/300Traininglasso_new_weight_vec10_insects.out'
+cat = 'insects'
+#data = mmread(fname)
+data = np.loadtxt(fname)
 print np.shape(data)
-rank = colmeans(data[275:300,:])
-voxel_on_or_off = rank >=(21764-min_rank)
-plotsignificantbrain(coords, rank)
+#rank = colmeans(data[275:300,:])
+#voxel_on_or_off = rank >=(21764-min_rank)
+#plotsignificantbrain(coords, rank)
+plotsignificantbrain(coords, data, cat)
